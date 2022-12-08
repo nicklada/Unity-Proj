503cd8722aa4f2922e0b7956677cac24ad311e01
lada <nicklada94@gmail.com>
2022-12-09 00:19:09 +0300

    lab 7 added

---
+++ Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections; // Подключение основных расширений (коллекций)
+using System.Collections.Generic; // Подключение дополнительных коллекций
+using UnityEngine; //Подключение основных функций Unity
+
+public class CharacterAnimation : MonoBehaviour // Класс CharacterAnimatio (название скрипта) наследует класс MonoBehaviour
+{
+    private ProgrammManager ProgrammManagerScript; // Создание переменной под скрипт ProgrammManager, чтобы в дальнейшем на него ссылаться
+    private Animator anim; // Создание переменной anim, которая будет являться ссылкой на компонент аниматора (Animator) 
+
+    void Start()
+    {
+        ProgrammManagerScript = FindObjectOfType<ProgrammManager>(); // Создание переменной под скрипт ProgrammManager, чтобы в дальнейшем на него ссылаться
+        anim = GetComponent<Animator>(); // Поиск и запись найденного компонента Animator в переменную anim (запись действий из аниматора в переменую)
+    }
+
+    void Update()
+    {
+        if (ProgrammManagerScript.CharacterAnimation) // Условие выполняется, если значение булевой переменной CharacterAnimation - true ("летающий" виртуальный экран появляется на сцене, нажата кнопка "Диалог")
+        {
+            anim.SetBool("talking", true); // Срабатывает bool "talking", и ассистент сменяет анимацию покоя "Idol" на "разговорное состояние - talking/talking1"
+        }
+        else // Выполняется, если значение булевой переменной CharacterAnimation - false (кнопка "Диалог" не нажата)
+        {
+            anim.SetBool("talking", false); // Анимация объекта возвращается в исходное состояние "Idol" из состояния "talking/talking1"
+        }
+    }
+}
