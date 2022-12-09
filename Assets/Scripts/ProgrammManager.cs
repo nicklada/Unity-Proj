4deacd5bcd2340d0dfbd5a8a6e111a5a8859a975
lada <nicklada94@gmail.com>
2022-12-09 19:28:27 +0300

    lab 7 added and fixed

--- Assets/Scripts/ProgrammManager.cs
+++ Assets/Scripts/ProgrammManager.cs
@@ -1,175 +1,162 @@
-﻿using System.Collections; // Подключение основных расширений (коллекций)
-using System.Collections.Generic; // Подключение дополнительных коллекций
-using UnityEngine; //Подключение основных функций Unity
-using UnityEngine.XR.ARFoundation; // Библиотека для работы ARCore
-using UnityEngine.XR.ARSubsystems; // Библиотека для работы ARCore
-
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.XR.ARFoundation; 
+using UnityEngine.XR.ARSubsystems; 
 
 public class ProgrammManager : MonoBehaviour
 {
     [Header("Put your planeMarker here")]
-    [SerializeField] private GameObject PlaneMarkerPrefab; // Контейнер PlaneMarkerPrefab для маркера
-    private ARRaycastManager ARRaycastManagerScript; // Ссылка на скрипт AR Raycast Manager для получения из него лучей
-    private Vector2 TouchPosition; // Создание двумерной переменой TouchPosition
+    [SerializeField] private GameObject PlaneMarkerPrefab; 
+    private ARRaycastManager ARRaycastManagerScript; 
+    private Vector2 TouchPosition; 
     public GameObject ObjectToSpawn;
-    public bool ChooseObject = false; // Создание булевой переменной ChooseObject со значением false, т.е. изначально никакой объект не выбран
+    public bool ChooseObject = false; 
     [Header("Put ScrollView here")]
-    public GameObject ScrollView; // Создание ячейки под ScrollView
-    [SerializeField] private Camera ARCamera; // Создание ячейки под ARCamera
-    List<ARRaycastHit> hits = new List<ARRaycastHit>(); // Создание фрейм-контейнера hits, как переменную для уменьшения нагрузки на дальнейший расчет 
-    private GameObject SelectedObject; // Создание ячейки SelectedObject под выбранный объект, чтобы в дальнейшем менять его параметры
-    public bool Moving; // Создание булевой переменной Moving, которая отвечает за то, перемещает ли пользователь объект или нет (нажата кнопка "Переместить" или нет)
-    public bool Rotation; // Создание булевой переменной Rotation, которая отвечает за то, вращает ли пользователь объект или нет (нажата кнопка "Вращение" или нет)
-    private Quaternion YRotation; // Создание переменной вращения YRotation (так как вращаем объект только по координате "Y")
-    public bool Communication; // Создание булевой переменной Communication, которая отвечает за то, нажата ли кнопка "Диалог" или нет
+    public GameObject ScrollView; 
+    [SerializeField] private Camera ARCamera; 
+    List<ARRaycastHit> hits = new List<ARRaycastHit>(); 
+    private GameObject SelectedObject; 
+    public bool Moving; 
+    public bool Rotation; 
+    private Quaternion YRotation; 
+    public bool Communication; 
     [Header("В ячейку перенеси префаб Ассистента")]
-    public GameObject VirtualDisplayPrefab; // Создание ячейки под префаб "летающего" виртуального экрана
-    private GameObject VirtualDisplay; // Создание ячейки VirtualDisplay под префаб VirtualDisplayPrefab, чтобы в дальнейшем менять его параметры
+    public GameObject VirtualDisplayPrefab; 
+    private GameObject VirtualDisplay; 
     [Header("В ячейку перенесите позицию Ассистента")]
-    public Transform VirtualDisplayPosition; // Создание ячейки под позицию виртуального экрана VirtualDisplayPosition для того, чтобы в дальнейшем управлять его движением  
-    public bool CharacterAnimation = false; // Создание булевой переменной CharacterAnimation со значением false, т.е. изначально у объекта включена анимация "Idol"  
+    public Transform VirtualDisplayPosition; 
+    public bool CharacterAnimation = false;
 
     void Start()
     {
-        ARRaycastManagerScript = FindObjectOfType<ARRaycastManager>(); // Поиск в проекте компонента AR Raycast Manager, из которого в дальнейшем можно получить нужную информацию
-        PlaneMarkerPrefab.SetActive(false); // Маркер в самом начале не виден
-        ScrollView.SetActive(false); // При запуске приложения ScrollView не отображается
+        ARRaycastManagerScript = FindObjectOfType<ARRaycastManager>(); 
+        PlaneMarkerPrefab.SetActive(false);
+        ScrollView.SetActive(false); 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if (ChooseObject) // Если пользователь нажал на иконку с объектом, то ChooseObject принимает значение true, соответственно, функция начинает выполняться, на плоскости появляется маркер и пользователю предоставляется возможность установить объект
+        if (ChooseObject) 
         {
-            ShowMarkerAndSetObject(); // Вызов функции ShowMarkerAndSetObject
+            ShowMarkerAndSetObject(); 
         }
-
-        MoveObjectAndRotation(); // Объявление функции MoveObjectAndRotation
-        ShowVirtualDisplay(); // Объявление функции ShowVirtualDisplay
-        StopVirtualDisplay(); // Объявление функции StopVirtualDisplay
+        MoveObjectAndRotation(); 
+        ShowVirtualDisplay(); 
+        StopVirtualDisplay(); 
 
         // Необходимо, чтобы виртуальный экран следовал за пользователем и всегда был в его поле зрения
         // Проверка на дистанцию
-        if (CheckDist() >= 0.1f) // Условие выполняется, если дистанция от пользователя до виртуального экрана >= 0.1f (float)
+        if (CheckDist() >= 0.1f) 
         {
-            MoveObjToPos(); // Вызов функции MoveObjToPos при выполнении условия на дистанцию
+            MoveObjToPos(); 
         }
 
-        VirtualDisplay.transform.LookAt(ARCamera.transform); // Поворот виртуального дисплея в ту сторону, куда смотрит камера (ARCamera) устройства пользователя
+        VirtualDisplay.transform.LookAt(ARCamera.transform); 
     }
-
     void ShowMarkerAndSetObject()
     {
-        List<ARRaycastHit> hits = new List<ARRaycastHit>(); // Создание фрейм-контейнера hits, в который будут помещаться объекты, попадающиеся на пути следования луча, а именно все пересечения плоскости с лучами
-        ARRaycastManagerScript.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes); // Запуск луча из центра экрана (деление ширины и высоты экрана на 2); место хранения информации о пересечении луча с плоскостью - hits; фиксация плоскостей
+        List<ARRaycastHit> hits = new List<ARRaycastHit>(); 
+        ARRaycastManagerScript.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes); 
 
         //Появление маркера
-        if (hits.Count > 0) // Срабатывает в случае, если луч пересек плоскость
+        if (hits.Count > 0) 
         {
-            PlaneMarkerPrefab.transform.position = hits[0].pose.position; // Присваиваем позиции маркера значение места, где луч пересекся с плоскостью - hits
-            PlaneMarkerPrefab.SetActive(true); // Маркер появляется, становится активным
+            PlaneMarkerPrefab.transform.position = hits[0].pose.position; 
+            PlaneMarkerPrefab.SetActive(true); 
         }
-
         //Установка объекта
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) // Условие хотя бы одного нажатия на экран; тестирование фазы начала: произошло ли нажатие на экран
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) 
         {
-            Instantiate(ObjectToSpawn, hits[0].pose.position, ObjectToSpawn.transform.rotation); // Установка объекта (виртуального ассистента) в место пересечения луча с плоскостью; объект устанавливается той стороной (вращение), которой он стоит в проекте 
+            Instantiate(ObjectToSpawn, hits[0].pose.position, ObjectToSpawn.transform.rotation); 
             ChooseObject = false;
-            PlaneMarkerPrefab.SetActive(false); // После установки объекта пользователем, маркер становится невидимым
+            PlaneMarkerPrefab.SetActive(false); 
         }
     }
 
-    void MoveObjectAndRotation() // Функция, отвечающая за перемещение и вращение объекта
+    void MoveObjectAndRotation() 
     {
-        if (Input.touchCount > 0) // Отслеживание нажатия пальца на экран
+        if (Input.touchCount > 0) 
         {
             // Отслеживание места нажатия пальца на экран
-            Touch touch = Input.GetTouch(0); // Получение информации о касании экрана и её запись в переменную touch
-            TouchPosition = touch.position; // Помещение в двумерную переменную TouchPosition координат позиции нажатия пользователя на экран устройства
-
-            if (touch.phase == TouchPhase.Began) // Условие, которое выполняется только в тот момент, когда пользователь коснулся экрана устройства пальцем, т.е. в самом начале
+            Touch touch = Input.GetTouch(0); 
+            TouchPosition = touch.position; 
+            if (touch.phase == TouchPhase.Began) 
             {
-                Ray ray = ARCamera.ScreenPointToRay(touch.position); // Запуск луча, фиксирующего объекты на своём пути, из места касания экрана пальцем пользователя
-                RaycastHit hitObject; // Создание контейнера hitObject, хранящего информацию об объектах, которых пересек луч
-
-                if (Physics.Raycast(ray, out hitObject)) // Создание условия, которое выполняется при пересечении лучом объекта 
+                Ray ray = ARCamera.ScreenPointToRay(touch.position); 
+                RaycastHit hitObject; 
+                if (Physics.Raycast(ray, out hitObject)) 
                 {
-                    if (hitObject.collider.CompareTag("UnSelected")) // Условие выполняется, если пересеченный лучом объект имеет тег "UnSelected"
+                    if (hitObject.collider.CompareTag("UnSelected")) 
                     {
-                        hitObject.collider.gameObject.tag = "Selected"; // Невыделенному объекту с тегом UnSelected присваивается тег "Selected" - объект выбран
+                        hitObject.collider.gameObject.tag = "Selected"; 
                     }
                 }
             }
-
-            SelectedObject = GameObject.FindWithTag("Selected"); // Помещаем в ячейку SelectedObject объект, у которого есть тег "Selected"   
-
-            if (touch.phase == TouchPhase.Moved && Input.touchCount == 1) // Условие выполняется при движении пальца по экрану (фаза движения) и количестве касаний = 1 (коснулись одним пальцем, а не двумя)
+            SelectedObject = GameObject.FindWithTag("Selected"); 
+            if (touch.phase == TouchPhase.Moved && Input.touchCount == 1) 
             {
-                if (Moving) // Условие выполняется, если значение булевой переменной Moving - true (нажата кнопка "Переместить")
+                if (Moving) 
                 {
-                    ARRaycastManagerScript.Raycast(TouchPosition, hits, TrackableType.Planes); // Запуск лучей из места экрана, куда пользователь коснулся пальцем; полученная информация о пересечении луча с полоскостью помещается в контейнер hits; отслеживание плоскостей                   
-                    SelectedObject.transform.position = hits[0].pose.position; // У объекта, помещенного в ячейку SelectedObject (объект с тегом "Selected"), меняем позицию на то место, где луч пересекся с плоскостью
+                    ARRaycastManagerScript.Raycast(TouchPosition, hits, TrackableType.Planes); 
+                    SelectedObject.transform.position = hits[0].pose.position; 
                 }
-
-                if (Rotation) // Условие выполняется, если значение булевой переменной Rotation - true (нажата кнопка "Вращение")
+                if (Rotation) 
                 {
-                    YRotation = Quaternion.Euler(0f, -touch.deltaPosition.x * 0.1f, 0f); // Переменной YRotation присваивается значение, которое состоит из трех координат вращения (x,y,z). Положение меняется в зависимости от движения пальца по экрану
-                    SelectedObject.transform.rotation = YRotation * SelectedObject.transform.rotation; // Меняем позицию вращения выбранного объекта путем её умножения на переменную YRotation, содержащую в себе углы
+                    YRotation = Quaternion.Euler(0f, -touch.deltaPosition.x * 0.1f, 0f); 
+                    SelectedObject.transform.rotation = YRotation * SelectedObject.transform.rotation; 
                 }
             }
-
-            if (touch.phase == TouchPhase.Ended) // Условие, выполняющееся в тот момент, когда пользователь отпустил палец от экрана (фаза конца)
+            if (touch.phase == TouchPhase.Ended) 
             {
-                if (SelectedObject.CompareTag("Selected")) // Условие выполняется, если существует объект с тегом "Selected"
+                if (SelectedObject.CompareTag("Selected")) 
                 {
-                    SelectedObject.tag = "UnSelected"; // Присваиваем объекту с тегом "Selected" обратно тег "UnSelected" - объект не выбран
+                    SelectedObject.tag = "UnSelected"; 
                 }
             }
         }
     }
 
-    void ShowVirtualDisplay() // Функция появления виртуального экрана при нажатии на установленного в пространстве ассистента
+    void ShowVirtualDisplay() 
     {
-        if (Input.touchCount > 0) // Ослеживание нажатия пальца на экран
+        if (Input.touchCount > 0) 
         {
-            Touch touch = Input.GetTouch(0); // Получение информации о касании экрана и её запись в переменную touch
-            TouchPosition = touch.position; // Помещение в двумерную переменную TouchPosition координат позиции нажатия пользователя на экран устройства
-
-            if (touch.phase == TouchPhase.Began) // Условие, которое выполняется только в тот момент, когда пользователь коснулся экрана устройства пальцем, т.е. в самом начале 
+            Touch touch = Input.GetTouch(0); 
+            TouchPosition = touch.position; 
+            if (touch.phase == TouchPhase.Began) 
             {
-                Ray ray = ARCamera.ScreenPointToRay(touch.position); // Запуск луча, фиксирующего объекты на своём пути, из места касания экрана пальцем пользователя 
-                RaycastHit hitObject; // Создание контейнера hitObject, хранящего информацию об объектах, которых пересек луч
-
-                if (Physics.Raycast(ray, out hitObject)) // Создание условия, которое выполняется при пересечении лучом объекта 
+                Ray ray = ARCamera.ScreenPointToRay(touch.position); 
+                RaycastHit hitObject; 
+                if (Physics.Raycast(ray, out hitObject)) 
                 {
-                    if (Communication && CharacterAnimation == false) // Условие выполняется, если значение булевой переменной Communication - true(нажата кнопка "Диалог") и анимация ассистента в режиме "Idol"(для предотвращения появления нескольких виртуальных экранов)
+                    if (Communication && CharacterAnimation == false) 
                     {
                         // Появление "летающего" виртуального экрана
-                        VirtualDisplay = Instantiate(VirtualDisplayPrefab, ARCamera.transform.position + new Vector3(0, 1f, 0), ARCamera.transform.rotation); // Появление объекта VirtualDisplayPrefab (модели виртуального экрана); позиция "летающего" экрана при его появлении на один метр выше камеры пользователя, чтобы создать эффект анимации (экран опустится сверху в видимую область); вращение объекта
-                        CharacterAnimation = true; // Присваиваем переменной CharacterAnimation значение true для того, чтобы анимация ассистента сменилась на "talking" (разговорное состояние)                                                                                                                                      // 
+                        VirtualDisplay = Instantiate(VirtualDisplayPrefab, ARCamera.transform.position + new Vector3(0, 1f, 0), ARCamera.transform.rotation); 
+                        CharacterAnimation = true;
                     }
                 }
             }
         }
     }
-
-    public float CheckDist() // Функция, считающая расстояние между камерой и виртуальным экраном
+   
+ public float CheckDist() 
     {
-        float dist = Vector3.Distance(VirtualDisplay.transform.position, VirtualDisplayPosition.transform.position); // Запись дистанции между позицией виртуального экрана и позицией камеры (ARCamera) в переменную dist
-        return dist; // Возвращается значение dist
+        float dist = Vector3.Distance(VirtualDisplay.transform.position, VirtualDisplayPosition.transform.position); 
+        return dist; 
+    }
+    
+private void MoveObjToPos() 
+    {
+        VirtualDisplay.transform.position = Vector3.Lerp(VirtualDisplay.transform.position, VirtualDisplayPosition.position, 1f * Time.deltaTime); 
     }
 
-    private void MoveObjToPos() // Функция, меняющая позицию виртуального экрана при выполнении условия на дистанцию
+    private void StopVirtualDisplay() 
     {
-        VirtualDisplay.transform.position = Vector3.Lerp(VirtualDisplay.transform.position, VirtualDisplayPosition.position, 1f * Time.deltaTime); // Меняется позиция виртуального экрана путем использования функции сокращения расстояния между двумя объектами (позицией экрана и позицией камеры); с какой скоростью меняется позиция
-    }
-
-    private void StopVirtualDisplay() // Функция, которая выполняется при выключении кнопки "Диалог"
-    {
-        if (Communication == false) // Условие выполняется, если значение булевой переменной Communication - false (пользователь выключает кнопку "Диалог") 
+        if (Communication == false) 
         {
-            VirtualDisplay.SetActive(false); // Виртуальный "летающий" экран пропадает с игровой сцены
-            CharacterAnimation = false; // Присваиваем переменной CharacterAnimation значение false для того, чтобы анимация ассистента вернулась в исходное состояние "Idol" (состояние покоя)
+            VirtualDisplay.SetActive(false);
+            CharacterAnimation = false; 
         }
     }
 }
+
